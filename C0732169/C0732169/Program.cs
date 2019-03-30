57035b471755efa6fbeb0b18a703938964e3f306
Gurkirat Singh <732169@student.tor.loc>
2019-03-29 20:36:33 -0400

    8:20

--- C0732169/C0732169/Program.cs
+++ C0732169/C0732169/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace C0732169
 {
@@ -10,6 +11,19 @@ namespace C0732169
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("downloading a file");
+            Download();
+            Console.ReadLine();
         }
+
+        static void Download()
+        {
+<<<<<<< HEAD
+            Thread.Sleep(3000);
+            Console.WriteLine("Download complete...");
+        }
+=======
+        Thread.Sleep(7000);
+>>>>>>> 47f8a5d902d6c55f3157ddbd3474df455032f3ec
     }
 }
